613db59cf0e7a0c3625a3e428a0e6ebb9bfba9b2
codease <AnyDark@163.com>
2015-05-04 16:29:39 +0800

    just 4 test

--- src/main/java/com/centling/demo/HomeController.java
+++ src/main/java/com/centling/demo/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-
+ 	/* for test */
 
 	/**
 	 * Simply selects the home view to render by returning its name.
